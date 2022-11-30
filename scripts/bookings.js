58e991ab366bdca7c5ff6797641a3c87f24586b7
Will Hedges <wshedges@outlook.com>
2022-11-30 15:55:52 -0600

    added booking filter and html functions

--- scripts/bookings.js
+++ scripts/bookings.js
@@ -1,6 +1,40 @@
-import { getBookings } from "./database.js";
+import { getBands, getBookings, getVenues } from "./database.js";
+
+const bands = getBands();
+const bookings = getBookings();
+const venues = getVenues();
+
+// pull out a band based on a booking band id
+const getBandByBandId = (bookingObj) => {
+  for (const band of bands) {
+    if (bookingObj.bandId == band.id) {
+      return band;
+    }
+  }
+};
+
+// pull out a venue based on a booking venue id
+const getVenueByVenueId = (bookingObj) => {
+  for (const venue of venues) {
+    if (bookingObj.venueId == venue.id) {
+      return venue;
+    }
+  }
+};
 
 // make and export a bulleted list of all bookings
-const bookings = getBookings();
+export const bookingsHTML = () => {
+  let html = "<ul>";
+  let [band, venue] = ["", ""];
 
-export const bookingsHTML = () => {};
+  // go through all the bookings
+  for (const booking of bookings) {
+    // look at the booking, and match the band id
+    band = getBandByBandId(booking);
+    // look at the booking, and match the venue id
+    venue = getVenueByVenueId(booking);
+    // create a string with the pertinent information
+    html += `<li id="booking--${booking.id}">${band.name} is playing at ${venue.name} on ${booking.date}</li>`;
+  }
+  return html;
+};
